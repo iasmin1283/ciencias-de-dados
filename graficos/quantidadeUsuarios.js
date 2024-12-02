d0580cca527fa7b9fc56dc0347833f5fdac8ccca
iasmin1283 <163189544+iasmin1283@users.noreply.github.com>
2024-12-02 08:33:02 -0300

    ....

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,6 +1,5 @@
 async function quantidadeUsuarios() {
-    const url = ' 
-    https://cdn.plot.ly/plotly-2.27.0.min.js '
+    const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-dados-gerais.json'
     const res = await fetch(url)
     const dados = await res.json()
     const pessoas_mundo = Object.keys(dados)
